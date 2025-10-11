701d604312ac97677ba417b1a01ae6feccf056b8
tiensirobloxytb-creator <tiensirobloxytb@gmail.com>
2025-10-11 22:16:40 +0700

    Update script pvb.lua

--- script pvb.lua
+++ script pvb.lua
@@ -2,7 +2,7 @@ script_key="QCjsJWXRTLfQQWYdGnhqMgeeRSGuCgyn";
 
 getgenv().pvbConfig = {
     AUTO_UPDATE_RESTART = true,
-    MAX_FPS = 3,  -- This will override setfpscap()
+    MAX_FPS = 2,  -- This will override setfpscap()
     LOW_CPU = true,
     MAX_REBIRTH = 2,  -- Stop rebirth at set amount
     FORCE_REBIRTH_IGNORE_KEEP_BRAINROT = true,  -- Ignore KEEP_BRAINROT related config until max rebirth
@@ -11,19 +11,19 @@ getgenv().pvbConfig = {
     OPEN_LUCKY_EGG = {"Godly Lucky Egg", "Secret Lucky Egg", "Meme Lucky Egg"},
     FUSE_PLANT = {"Mr Carrot", "Pumpkin", "Sunflower", "Dragon Fruit", "Eggplant", "Watermelon"},  -- Auto keep & fuse required plant + brainrot
 
-    BUY_SEED_SHOP = {["Cactus"] = 5, ["Strawberry"] = 5, ["Pumpkin"] = 5, ["Sunflower"] = 5, ["Dragon Fruit"] = 5, ["Eggplant"] = 5, ["Watermelon"] = 5, "Cocotank", "Carnivorous Plant", "Mr Carrot", "Tomatrio", "Shroombino", "Mango"},
+    BUY_SEED_SHOP = {["Cactus"] = 5, ["Strawberry"] = 5, ["Pumpkin"] = 5, ["Sunflower"] = 5, ["Dragon Fruit"] = 5, ["Eggplant"] = 5, ["Watermelon"] = 5, "Cocotank", "Carnivorous Plant", "Mr Carrot", "Tomatrio", "Shroombino", "Mango", "King Limone"},
     BUY_GEAR_SHOP = {"Frost Grenade", "Frost Blower"},
     KEEP_SEED = {},
     KEEP_PLANT_RARITY = {"Secret", "Limited"},
-    KEEP_BRAINROT_MONEY_PER_SECOND = 20000,  -- Number
+    KEEP_BRAINROT_MONEY_PER_SECOND = 50000,  -- Number
     KEEP_BRAINROT_RARITY = {},
 
-    SELL_BRAINROT_DELAY = 30,
-    SELL_PLANT_DELAY = 30,
+    SELL_BRAINROT_DELAY = 20,
+    SELL_PLANT_DELAY = 20,
 
     -- Webhook
-    BRAINROT_WEBHOOK_URL = "https://discord.com/api/webhooks/1289439977494810634/pWnYXsBkBzp79GYswy5YPvQmzYQajiwE30ax2MQEqlaY7DqgzKp6-Nau62Lo_fBBx2Jd",
-    DISCORD_ID = "888615724078231593",
+    BRAINROT_WEBHOOK_URL = "",
+    DISCORD_ID = "",
     NOTIFY_RARITY = {},
     NOTIFY_MONEY_PER_SECOND = 10000,
     WEBHOOK_NOTE = "",
